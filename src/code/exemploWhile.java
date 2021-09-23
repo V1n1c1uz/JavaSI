ecda0e9ea65842b5a1d6bb490ed57c91d097ec0b
V1n1c1uzDev <viniciusdesigner0@gmail.com>
2021-09-22 23:20:36 -0300

    #feat - Exemplo while

---
+++ src/code/exemploWhile.java
@@ -0,0 +1,16 @@
+package code;
+
+public class exemploWhile {
+
+  public static void main(String args[]) {
+
+    int contador;
+    contador = 0;
+
+    while (contador <= 10) {
+      System.out.println("Hello, world!");
+      contador++;
+
+    }
+  }
+}
