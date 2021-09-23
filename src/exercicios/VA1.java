4fbad0ece13f74c6299085406a81f83ae9fd9527
V1n1c1uzDev <viniciusdesigner0@gmail.com>
2021-09-22 23:24:55 -0300

    #refactor - Adição exercicio 1 VA1

--- src/exercicios/VA1.java
+++ src/exercicios/VA1.java
@@ -1,11 +1,54 @@
-/* Desenvolva um programa em java ou Portugol contendo os seguintes requisitos.
-  a) Informe o primeiro nome
-  b) Informe o segundo nome
-  c) Imprima o nome completo
- */
+/*
+1) Desenvolva um programa em Java ou Portugol contendo os seguintes requisitos:
+
+a) Calcular a média de 6 notas de alunos.
+b) identificar qual é a menor e maior nota.
+
+*/
 
 package exercicios;
 import java.util.Scanner;
+
+class calcularMedia {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+ 
+    int contador = 0, resultado = 0, media = 0, maior = 0, menor = 999999;
+ 
+    while (contador <= 5) {
+      contador = contador + 1;
+      System.out.print("Digite sua nota: ");
+      int nota = scanner.nextInt();
+ 
+      resultado = resultado + nota;
+      media = resultado / 6;
+ 
+      if (nota > maior) {
+        maior = nota;
+      }
+      if (nota <= menor) {
+        menor = nota;
+      }
+ 
+    }
+ 
+    System.out.print("As media das notas é: " + media);
+    System.out.print("\nA maior nota: " + maior);
+    System.out.print("\nMenor nota: " + menor);
+    scanner.close();
+  }
+}
+
+
+/* 
+2) Desenvolva um programa em java ou Portugol contendo os seguintes requisitos.
+
+a) Informe o primeiro nome
+b) Informe o segundo nome
+c) Imprima o nome completo
+
+ */
+
 
 public class VA1 {
   public static void main(String[] args) {
