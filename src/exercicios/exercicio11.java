19de5a72f9b7da94b7509f3e523b695929867a84
V1n1c1uzDev <viniciusdesigner0@gmail.com>
2021-09-27 20:57:37 -0300

    #refactor - Exercicio11

--- src/exercicios/exercicio11.java
+++ src/exercicios/exercicio11.java
@@ -5,24 +5,33 @@ import java.util.Scanner;
 public class exercicio11 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int temperatura, matricula = 0;
 
-
-
-    while(matricula != -1){
+    int temperatura, matricula = 0, totalPositivo = 0, totalSuspeito = 0;
+    String TeveContato;
+    while (matricula != -1) {
 
       System.out.println("Por favor informe sua matricula: ");
       matricula = scanner.nextInt();
 
-      System.out.println("Por favor digite sua temperatura: ");
-      temperatura = scanner.nextInt();
+      if (matricula != -1) {
 
-      if(temperatura > 37){
-        System.out.println("É maior");
+        System.out.println("Por favor digite sua temperatura: ");
+        temperatura = scanner.nextInt();
+
+        if (temperatura > 37) {
+
+          System.out.println("Você teve algum contato com uma pessoa com covid? ");
+          TeveContato = scanner.next().toLowerCase();
+
+          if (TeveContato.equals("sim")) {
+            totalPositivo = totalPositivo + 1;
+          } else {
+            totalSuspeito = totalSuspeito + 1;
+          }
+        }
       }
     }
-
-
-
+    System.out.println("Quantidade de alunos com suspeita: " + totalSuspeito + " Quantidade de alunos com COVID positivo: " + totalPositivo);
+    scanner.close();
   }
 }
