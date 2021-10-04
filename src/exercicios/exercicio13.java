3aa8a0b2b14ecafb63b567caa391c4de049d6e6d
V1n1c1uzDev <viniciusdesigner0@gmail.com>
2021-10-04 20:45:31 -0300

    #feat - Exercicio 13

---
+++ src/exercicios/exercicio13.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio13 {
+  public static void main(String[] args){
+   
+  int nota, soma = 0, media =0 ;
+  Scanner scanner = new Scanner(System.in);
+
+  for (int i = 1; i <= 12; i++) {
+    System.out.println("Informe a nota:");
+    nota = scanner.nextInt();
+    soma = soma + nota;
+    media = soma / 12;
+    }
+    scanner.close();
+    System.out.println("A media é: "+ media);
+  }
+}
